9ea62b50db9b1d2b9a504e23238eb1a6a7e13b8a
Ignas Sadonis <ignassadonis@gmail.com>
2016-08-26 20:56:42 +0200

    The main class now implements the plugin base

---
+++ PluginBase/BotManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using PluginInterface;
+
+namespace PluginBase
+{
+    public class BotManager : IBot
+    {
+        public int version { get; set; } = 5;
+        public string data { get; set; } = "this is some info.";
+
+        public void DoAction()
+        {
+            Console.WriteLine("Doing action!");
+        }
+
+        public bool DoAnotherAction(string var1, int var2)
+        {
+            Console.WriteLine(var2 + "\t |" + var1);
+            return true;
+        }
+    }
+}
