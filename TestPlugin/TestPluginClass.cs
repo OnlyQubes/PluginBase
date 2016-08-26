489255fe44f77fc10460545a4a4f6365432d0e9f
Ignas Sadonis <ignassadonis@gmail.com>
2016-08-26 21:04:17 +0200

    this  with '#' will be ignored, and an empty message aborts the commit.

---
+++ TestPlugin/TestPluginClass.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PluginInterface;
+
+namespace TestPlugin
+{
+    public class TestPluginClass : IPlugin
+    {
+        private IBot mainClass;
+
+        public string GetName()
+        {
+            return "Test plugin";
+        }
+
+        public string GetAuthor()
+        {
+            return "John";
+        }
+
+        public int GetVersion()
+        {
+            return 1;
+        }
+
+        public void OnLoaded(IBot mainClass)
+        {
+            this.mainClass = mainClass;
+
+            //Call some test functions.
+            mainClass.DoAction();
+            this.mainClass.DoAnotherAction("Test plugin doing actions!", GetVersion());
+        }
+    }
+}
