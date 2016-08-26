30fa0c00b3a40573e9908891fb939f9d67b5ad42
Ignas Sadonis <ignassadonis@gmail.com>
2016-08-26 21:02:02 +0200

    Added an explanation on how this should be used

---
+++ PluginInterface/IPlugin.cs
@@ -0,0 +1,16 @@
+﻿namespace PluginInterface
+{
+    /*
+        This should be implemented
+        only by the plugins.
+    */
+
+    public interface IPlugin
+    {
+        string GetName();
+        string GetAuthor();
+        int GetVersion();
+
+        void OnLoaded(IBot mainClass);
+    }
+}
